d7337f627c123125c94fd96a29d4cd97e5effae5
Cole Becker <cbecker29@huskers.unl.edu>
2022-08-25 13:27:15 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Cole Becker
+ *         cbecker29@huskers.unl.edu
+ * Date: 08/23/2022
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Cole Becker\n");
+
+  return 0;
+}
